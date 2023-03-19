8c0c2382249791ca5133d5d465c5a8c0efd64e0d
mheydt <46801225+Bigskyy@users.noreply.github.com>
2023-03-20 10:45:23 +1100

    Initial sketch code

---
+++ Homework9/sketch.js
@@ -0,0 +1,26 @@
+var width = 800;
+var height = 4000.0;
+
+function setup() {
+    createCanvas(width*2, height);
+}
+
+
+
+function draw() {
+    background(220);
+    //circle(30,40,50);
+    //square(30,40,50);
+    //ellipse(30,40,50,60);
+    //rect(30,40,50,60);
+    //triangle(30, 75, 58, 20, 86, 75);
+    //point(30, 75);
+    //line(30, 20, 85, 75);
+    //text('Hello there!', 10, 30);
+    textSize(32);
+
+
+    circle()
+
+    text('Hello there!', 10, 30);
+  }
